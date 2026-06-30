0222852910c6b4bb689ed0520f97f328dc8bf3dc
thiagoabreu46 <thiagoabreu293@gmail.com>
2026-06-29 22:49:19 -0300

    formatacao colorida do texto

--- modeline.lisp
+++ modeline.lisp
@@ -1,12 +1,11 @@
 ;;  config da modeline  ;;
 
 (in-package :stumpwm)
-(setf *window-format* "%m%n%s%c")
-(setf *screen-mode-line-format* (list "[^B%n^b] %W^>%d"))
+(load-module "cpu")
+(setf *window-format* "%n%s%c")
+(setf cpu::*cpu-modeline-fmt* "%c %f")
+(setf *screen-mode-line-format* (list "[%g] [^B^b] %W^> | %m | ^B %C ^b ^3 | %d |"))
 (setf *time-modeline-string* "%a %b %e %k:%M")
-(setf *mode-line-background-color* "black")
-(setf *mode-line-foreground-color* "white")
-(setf *mode-line-border-color* "white")
 (setf *mode-line-border-width* 3)
 (setf *mode-line-timeout* 2)
 
