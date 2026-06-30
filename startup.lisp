a5980a76f63ad047bfa0cdc871d1eed2bf952bad
thiagoabreu46 <thiagoabreu293@gmail.com>
2026-06-29 22:50:26 -0300

    so o cliente mpd iniciando junto com o resto

--- startup.lisp
+++ startup.lisp
@@ -1,17 +1,17 @@
 (in-package :stumpwm)
-
+(load-module "mpd")
 (stumpwm:run-commands
    "exec feh --bg-scale $HOME/.stumpwm.d/stumppaper.png"
    "exec picom"
    "exec dunst"
    "exec mpd"
    )
-
+(stumpwm:run-commands
+ "mpd-connect")
 #||(stumpwm:run-commands
+   "gnew 1"
    "gnew 2"
    "gnew 3"
    "gnew 4"
-   "gnew 5"
 )   
 ||#
-
